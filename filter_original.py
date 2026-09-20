d262393507fb0cb6843993d5d11c9e56d954288c
bhhshavsgki-lang <bhhshavsgki@gmail.com>
2026-09-20 12:20:49 -0700

    Add subscription URLs for V2Ray configurations
    Added multiple subscription URLs for various V2Ray configurations.

---
+++ filter_original.py
@@ -0,0 +1,55 @@
+# One subscription URL per line. Lines starting with # are ignored.
+# All of these are public aggregators updated daily by bot (verified alive).
+
+# ===== Big all-in-one lists (VLESS + VMess + Trojan + SS together) =====
+https://raw.githubusercontent.com/Epodonios/v2ray-configs/main/All_Configs_Sub.txt
+https://raw.githubusercontent.com/SoliSpirit/v2ray-configs/main/all_configs.txt
+https://raw.githubusercontent.com/mahdibland/V2RayAggregator/master/sub/sub_merge.txt
+https://raw.githubusercontent.com/barry-far/V2ray-Config/main/All_Configs_Sub.txt
+https://raw.githubusercontent.com/nyeinkokoaung404/V2ray-Configs/main/All_Configs_Sub.txt
+https://raw.githubusercontent.com/MhdiTaheri/V2rayCollector/main/sub/mix
+https://raw.githubusercontent.com/Leon406/SubCrawler/master/sub/share/a11
+https://raw.githubusercontent.com/ts-sf/fly/main/v2
+https://raw.githubusercontent.com/free18/v2ray/main/v.txt
+https://raw.githubusercontent.com/ermaozi/get_subscribe/main/subscribe/v2ray.txt
+https://raw.githubusercontent.com/Pawdroid/Free-servers/main/sub
+https://raw.githubusercontent.com/peasoft/NoMoreWalls/master/list_raw.txt
+https://raw.githubusercontent.com/ripaojiedian/freenode/main/sub
+
+# ===== Per-protocol lists (thousands of entries each, fresh daily) =====
+https://raw.githubusercontent.com/Epodonios/v2ray-configs/main/Splitted-By-Protocol/vless.txt
+https://raw.githubusercontent.com/Epodonios/v2ray-configs/main/Splitted-By-Protocol/vmess.txt
+https://raw.githubusercontent.com/Epodonios/v2ray-configs/main/Splitted-By-Protocol/trojan.txt
+https://raw.githubusercontent.com/barry-far/V2ray-Config/main/Splitted-By-Protocol/vless.txt
+https://raw.githubusercontent.com/barry-far/V2ray-Config/main/Splitted-By-Protocol/vmess.txt
+https://raw.githubusercontent.com/barry-far/V2ray-Config/main/Splitted-By-Protocol/trojan.txt
+https://raw.githubusercontent.com/SoliSpirit/v2ray-configs/main/Protocols/vless.txt
+https://raw.githubusercontent.com/SoliSpirit/v2ray-configs/main/Protocols/vmess.txt
+https://raw.githubusercontent.com/SoliSpirit/v2ray-configs/main/Protocols/trojan.txt
+https://raw.githubusercontent.com/Leon406/SubCrawler/master/sub/share/vless
+https://raw.githubusercontent.com/Leon406/SubCrawler/master/sub/share/v2
+https://raw.githubusercontent.com/Leon406/SubCrawler/master/sub/share/tr
+https://raw.githubusercontent.com/MhdiTaheri/V2rayCollector/main/sub/vless
+https://raw.githubusercontent.com/MhdiTaheri/V2rayCollector/main/sub/vmess
+https://raw.githubusercontent.com/MhdiTaheri/V2rayCollector/main/sub/trojan
+https://raw.githubusercontent.com/V2RayRoot/V2rayConfig/main/Config/vless.txt
+https://raw.githubusercontent.com/V2RayRoot/V2rayConfig/main/Config/vmess.txt
+https://raw.githubusercontent.com/V2RayRoot/V2rayConfig/main/Config/trojan.txt
+
+# ===== Smaller / curated lists (fresh daily) =====
+https://raw.githubusercontent.com/V2RayRoot/V2rayConfig/main/Config/proxies.txt
+https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/main/README.md
+https://raw.githubusercontent.com/0xRadikal/Free-v2ray-Configs/main/README.md
+https://raw.githubusercontent.com/sakha1370/OpenRay/main/README.md
+https://raw.githubusercontent.com/ShatakVPN/ConfigForge-V2Ray/main/README.md
+https://raw.githubusercontent.com/zhuhaiuk/free-nodes/main/README.md
+https://raw.githubusercontent.com/MahanKenway/Freedom-V2Ray/main/README.md
+https://raw.githubusercontent.com/Argh94/Proxy-List/main/README.md
+https://raw.githubusercontent.com/mohamadfg-dev/telegram-v2ray-configs-collector/main/README.md
+https://raw.githubusercontent.com/Idolvpn/Automate-V2ray-Config-Collector/main/README.md
+https://raw.githubusercontent.com/SHAHBBBB/V2ray-collector/main/README.md
+https://raw.githubusercontent.com/jafarm83/ConfigV2Ray/main/README.md
+https://raw.githubusercontent.com/VovaplusEXP/p-configs/main/README.md
+https://raw.githubusercontent.com/ProblemTheCode/SylphNet-public/main/README.md
+https://raw.githubusercontent.com/kasesm/Free-Config/main/README.md
+https://raw.githubusercontent.com/mohammadaz2/v2rayConfigsForYou/main/README.md
